a97ffea6ad3a0f334e0b60b2595a76af0c8f2796
Michael <michael.hope@csiro.au>
2015-04-23 17:07:37 +1000

    fixing 2015 date display issue

--- grails-app/taglib/au/org/ala/biocache/hubs/appd/OccurrenceTagLib.groovy
+++ grails-app/taglib/au/org/ala/biocache/hubs/appd/OccurrenceTagLib.groovy
@@ -47,8 +47,10 @@ class OccurrenceTagLib {
                 span(style:'margin-left: 8px;') {
                     if (occurrence.eventDate) {
                         outputResultsLabel("Date: ", g.formatDate(date: new Date(occurrence.eventDate), format:"dd-MM-yyyy"), true)
-                    } else if (occurrence.occurrenceYear || occurrence.year) {
-                        outputResultsLabel("Year: ", g.formatDate(number: new Date(occurrence.occurrenceYear?:occurrence.year), format:"yyyy"), true)
+                    } else if (occurrence.occurrenceYear) {
+                        outputResultsLabel("Year: ", g.formatDate(number: new Date(occurrence.occurrenceYear), format:"yyyy"), true)
+                    } else if (occurrence.year) {
+                        outputResultsLabel("Year: ", occurrence.year, true)
                     }
                     if (occurrence.stateProvince) {
                         outputResultsLabel("State: ", alatag.message(code:occurrence.stateProvince), true)
